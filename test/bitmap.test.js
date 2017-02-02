cdfeb69aa0c6fd7808bee1411e1b7f49a3ae4617
meghanroserebecca <meghanroserebecca@gmail.com>
2017-02-01 20:38:00 -0800

    took in marty code & adapted; added .gitignore file

---
+++ test/bitmap.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const fs = require('fs');
+const BitmapHeader = require('../lib/bitmap-header.js');
+const BitmapTransformer = require('../lib/bitmap-transformer.js');
+
+it('test transform', done /*()*/ => {
+  const bitmap = new BitmapTransformer(buffer);
+  //other stuff, lots of other stuff
+})
