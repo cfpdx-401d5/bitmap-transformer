17756a545ca3b4077b715e53c19df530434329fd
meghanroserebecca <meghanroserebecca@gmail.com>
2017-02-02 11:46:17 -0800

    bitmap-transformer code completes & not throwing errors; test for header written, passed, broken, confirmed;

--- test/bitmap.test.js
+++ test/bitmap.test.js
@@ -3,7 +3,24 @@ const fs = require('fs');
 const BitmapHeader = require('../lib/bitmap-header.js');
 const BitmapTransformer = require('../lib/bitmap-transformer.js');
 
-it('test transform', done /*()*/ => {
-  const bitmap = new BitmapTransformer(buffer);
-  //other stuff, lots of other stuff
-})
+describe('inverts colors in a bitmap', () => {
+
+  let buffer = null;
+  before(done => {
+    fs.readFile('./non-palette-bitmap-test.bmp', (err, _buffer) => {
+      if(err) done(err);
+      else {
+        buffer = _buffer;
+        done();
+      }
+    });
+  });
+  
+  it('gives correct header data', () => {
+    const bmHeader = new BitmapHeader(buffer);
+    assert.equal(bmHeader.pixelOffset, 54);
+    assert.equal(bmHeader.bitsPerPixel, 24);
+    assert.equal(bmHeader.fileSize, 30054);
+    assert.equal(bmHeader.isPaletted, false);
+  })
+});
