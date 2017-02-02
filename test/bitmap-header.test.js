44494d0573a62e8612ced0ec4e967e3715f2710a
Clarefollett <chlaw101@gmail.com>
2017-02-01 18:59:51 -0800

    couldn't quite figure out the palette image yet so I decided to go through my code and try the non palette first and fix somethings up. I have two tests working currently

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -4,40 +4,54 @@ const BitmapHeader = require('../lib/bitmap-header');
 const BitmapTransform = require('../lib/bitmap-transform')
 const invert = require('../lib/invert-colors');
 
+let palette = null;
+let noPalette = null;
+
 describe('working with bitmaps', () => {
-    
-    let buffer = null;
+
     before(done => {
         fs.readFile('./test/palette-bitmap.bmp', (err, _buffer) => {
             if(err) done(err);
             else {
-                buffer = _buffer;
+                palette = _buffer;
                 done();
             }
         });
     });
-    it('reads the header', () => {
-        const header = new BitmapHeader(buffer);
-        assert.equal(header.fileSize, 11078);
-        assert.equal(header.isPaletted, true);
-        assert.equal(header.pixelOffset, 1078);
-        assert.equal(header.bitsPerPixel, 8);
-    })
-    it('test transform', done => {
-        const bitmap = new BitmapTransform(buffer);
-        bitmap.transform(invert);
 
-        fs.writeFile('./test/palette-output.bmp', buffer, (err) => {
+    before(done => {
+        fs.readFile('./test/non-palette-bitmap.bmp', (err, _buffer) => {
             if (err) done(err);
-            else done();
-        })
-        // const buffer = fs.readFileSync('./test/output.bmp');
-        // assert.deepEqual(bitmap.buffer, buffer);
-        
-        // fs.readFile('./test/output.bmp', (err, buffer) => {
-        //     assert.deepEqual(bitmap.buffer, buffer);
-        //     done();
-        // });
+            else {
+                noPalette = _buffer;
+                done();
+            }
+        });
     });
 
+
+    it('reads the header', () => {
+        const header = new BitmapHeader(noPalette);
+        assert.equal(header.fileSize, 30054);
+        assert.equal(header.isPaletted, false);
+        assert.equal(header.pixelOffset, 54);
+        assert.equal(header.bitsPerPixel, 24);
+    });
+});
+
+describe('invert transformation', () => {    
+    it('test transform', () => {
+        const bitmap = new BitmapTransform(noPalette);
+        const invertBuf = bitmap.transform(invert);
+            bitmap.write('./test/output.bmp', invertBuf, (err) => {
+                if (err) return err;
+                else {
+                    fs.readFile('./test/output.bmp', (err, buffer) => {
+                        assert.deepEqual(invertBuf, buffer);
+                        done();
+                    })
+                }
+        
+        });
+    });
 });
